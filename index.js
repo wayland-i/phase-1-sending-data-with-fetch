3226a29665c84148a009fa5fcf0f5c3ec11005d6
Ian Iversen <ianiversen1@gmail.com>
2022-07-07 19:12:34 -0400

    commit tree 8d515e5e62260503ae8f29a9958c1c5cdfdb8e59

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData (name, email) {
+
+
+    const formData = {
+        name,
+        email,
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(res => res.json())
+    .then(obj => obj);
+}
+
+ 
